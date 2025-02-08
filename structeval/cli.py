8b05af2b52df14eef45de10be76659d58cfa8b25
Jialin Yang <j586yang@uwaterloo.ca>
2025-02-07 19:08:49 -0500

    added inference code

--- structeval/cli.py
+++ structeval/cli.py
@@ -1,5 +1,7 @@
 import fire
 import os
+from inference import run_inference
+import json
 
 class StructEvalCLI:
     
@@ -9,12 +11,34 @@ class StructEvalCLI:
     
     def inference(
         self,
+        llm_model_name: str,
+        llm_engine: str,
         input_path: str,
         output_path: str,
-        **kwargs
+        **kwargs #temperatures, top_k, decoding parameters
     ):
-        print(f"Training model with input_path: {input_path} and output_path: {output_path}")
-        print(f"Additional arguments: {kwargs}")
+
+        with open(input_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        queries = [
+            f"{item['query']}\n\nIMPORTANT: Only output the executable code. Do NOT include any other things, such as explanations, comments, or markdown (```, **, #), or any formatting hints etc." 
+            for item in data
+        ]   
+
+        #define tags to represent output components : <code> </code>
+            
+        task_ids = [item["task_id"] for item in data] 
+
+        generations = run_inference(llm_model_name, llm_engine, queries)
+
+        
+        output_data = [{"task_id": tid, "query": q, "generation": gen} for tid, q, gen in zip(task_ids, queries, generations)]
+
+       
+        with open(output_path, "w", encoding="utf-8") as out_file:
+            json.dump(output_data, out_file, indent=2)
+
     
     def eval(
         self,
